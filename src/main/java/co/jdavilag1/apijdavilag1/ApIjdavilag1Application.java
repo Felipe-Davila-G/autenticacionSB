46b0877192a6e98da23fa4e6452384c290112bae
Juan Felipe Davila Galvis <jdavilag1@ucentral.edu.co>
2024-09-04 17:31:00 -0500

    primer commit al repositorio
    Signed-off-by: Juan Felipe Davila Galvis <jdavilag1@ucentral.edu.co>

---
+++ src/main/java/co/jdavilag1/apijdavilag1/ApIjdavilag1Application.java
@@ -0,0 +1,13 @@
+package co.jdavilag1.apijdavilag1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIjdavilag1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApIjdavilag1Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/jdavilag1/apijdavilag1/ApIjdavilag1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jdavilag1.apijdavilag1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIjdavilag1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
